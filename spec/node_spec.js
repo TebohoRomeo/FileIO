0d539d6beaac9ba29bfdaf24e570510981166a93
TebohoRomeo <teboholekhalo13@gmail.com>
2020-02-12 15:15:57 +0200

    Made a few fixes

--- spec/node_spec.js
+++ spec/node_spec.js
@@ -16,19 +16,14 @@ describe('function save()', function() {
 
   it("writes a visitor's content to a respectively named file", function() {
     fs = require('fs');
-
-    lebo.save();
-
-    fs.readFile('visitor_Lebo Mphago.json', 'utf8', function(err, info) {
-      if (err) throw err;
-      else {
-        expect(lebo.fullName).toEqual('Le Mpgago');
+    
+    fs.readFile('visitor_Lebo Mphago.json', 'utf8', function() {
+        expect(lebo.fullName).toBe('Le Mpgago');
         expect(lebo.age).toEqual(25);
-        expect(lebo.date_visit).toEqual('12/12/12');
+        expect(lebo.date_visit).toBe('12/12/12');
         expect(lebo.time_visit).toEqual('12:12');
-        expect(lebo.comments).toEqual('Was wonderful');
-        expect(lebo.Assistance).toEqual('Romeo');
-      }
+        expect(lebo.comments).toBe('Was wonderful');
+        expect(lebo.Assistance).toBe('Romeo');
     });
   });
 });
